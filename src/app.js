0cb12d007c47ff05e5a48c8fb6ba81357acf7758
EstebanFM <105824023+Esteban-FM@users.noreply.github.com>
2026-04-16 17:40:30 -0600

    servidor basico con express creado

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
-console.log("hola");
+import express from 'express';
+
+const app = express();
+
+
+// Middleware to parse JSON
+app.use(express.json());
+
+
+//Ruta de prueba
+// get es la peticion que hace el navegador hacia el servidor
+app.get('/', (req, res) => {
+    res.send('hola!')
+});
+
+//iniciar el servidor
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
+
